40a7ed9c849c509eb9995944780276dd8340597f
Janaki Pillai <janakipillai968300@gmail.com>
2024-12-13 15:24:58 +0530

    Create insertion_sort.c

---
+++ Cycle-8/insertion_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void Insetion_Sort(int arr[],int n)
+{
+    int j,key;
+    for(int i=1;i<n;i++)
+    {
+        key=arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>key)  
+        {
+            arr[j+1]=arr[j];    
+            j=j-1;            
+        }
+        arr[j+1]=key;
+    }
+
+}
+void main()
+{
+    int n,arr[25];
+    printf("Number of elements : ");
+    scanf("%d",&n);
+    printf("Enter the elements : ");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    Insetion_Sort(arr,n);
+    printf("After Sorting : ");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
